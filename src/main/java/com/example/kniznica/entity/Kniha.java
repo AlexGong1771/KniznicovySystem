6344b1e3f09f024e028f70d7b4a68dd5f1a2c5f8
Alex <alexthebestgong1771@gamil.com>
2025-01-12 12:53:08 +0100

    obnova knih teraz uz asi neda sa editovat knihu ze bude prazdna

--- src/main/java/com/example/kniznica/entity/Kniha.java
+++ src/main/java/com/example/kniznica/entity/Kniha.java
@@ -1,6 +1,8 @@
 package com.example.kniznica.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,13 +15,16 @@ public class Kniha {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @NotEmpty()
     private String nazov;
+    @NotEmpty()
     private String autor;
+    @NotEmpty()
     private String imageFileName;
 
 
     @Enumerated(EnumType.STRING)
+    @NotEmpty()
     private StavVypozicky jeVypozicana = StavVypozicky.NIE;
 
 
--- src/main/java/com/example/kniznica/entity/KnihaDto.java
+++ src/main/java/com/example/kniznica/entity/KnihaDto.java
@@ -3,20 +3,25 @@ package com.example.kniznica.entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
+
 @Getter
 @Setter
 public class KnihaDto {
+
     @NotEmpty(message = "This field is required")
-    private String nazov,autor ;
+    private String nazov;
+
+    @NotEmpty(message = "This field is required")
+    private String autor;
+
+    @NotEmpty(message = "Image file is required")
     private MultipartFile imageFile;
 
     @Enumerated(EnumType.STRING)
-    @NotEmpty(message = "ANO/NIE")
+    @NotNull(message = "ANO/NIE is required")
     private StavVypozicky jeVypozicanaDto = StavVypozicky.NIE;
-
-
-
 }
